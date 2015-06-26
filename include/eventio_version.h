8f48d94fc4c7399b96d23c7d002029faee4e3780
Christoph Deil <Deil.Christoph@gmail.com>
2015-06-26 15:27:08 +0200

    Initial commit

---
+++ include/eventio_version.h
@@ -0,0 +1,3 @@
+#define EVENTIO_VERSION "2015-06-15 19:41:12 CEST (konrad@wizard4)"
+#define EVENTIO_RELEASE "2015-06-15 (prod3 pre-production)"
+#define EVENTIO_BASE_RELEASE "2015-06-15 (prod3 pre-production)"
